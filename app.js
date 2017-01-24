dee6189c66dd328735997270b0d8110c724c57fb
mayamisaki <mayamisaki@gmail.com>
2017-01-24 17:15:36 +0000

    降順とランキング対応

--- app.js
+++ app.js
@@ -2,40 +2,50 @@
 const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input': rs, 'output': {} });
-
-const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (line) => {
-    const columns = line.split(',');
-    const year = columns[0];
+const rl = readline.createInterface({'input':rs, 'output':{}});
+const map = new Map();// key: 都道府県 value 集計データのオブジェクト
+rl.on('line', (lineString)=>{
+    //console.log(lineString);
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
     const prefecture = columns[2];
-    const popu = columns[7];
-    if (year === '2010' || year === '2015') {
+    const popu = parseInt(columns[7]);
+    if( year === 2010 || year === 2015)
+    {
+        //console.log(year);
+        //console.log(prefecture);
+        //console.log(popu);
         let value = map.get(prefecture);
-        if (!value) {
+        if(!value)
+        {
             value = {
-                p10: 0,
-                p15: 0,
-                change: null
-            };
+                p10 : 0,
+                p15:0,
+            change:null
+        };
+
         }
-        if (year === '2010') {
-            value.p10 += parseInt(popu);
+        if( year === 2010)
+        {
+            value.p10 += popu;
         }
-        if (year === '2015') {
-            value.p15 += parseInt(popu);
+        if( year === 2015)
+        {
+            value.p15 += popu;
         }
         map.set(prefecture, value);
     }
 });
 rl.resume();
-rl.on('close', () => {
-    for (let pair of map) {
+
+rl.on('close', ()=>{
+    for(let pair of map)
+    {
         const value = pair[1];
         value.change = value.p15 / value.p10;
     }
-    // TODO 減った割合のランキングにして順位も一緒に出力するようにしてください
-    const rankingArray = Array.from(map).sort((p1, p2) => {
+
+    const rankingArray = Array.from(map).sort((p1,p2)=>{
         return p1[1].change - p2[1].change;
     });
     const rankingStrings = rankingArray.map((p, i)=>{
