285118b378209f0918d20c10eb40493ed9db4b4e
kukey3 <frandre85563@gmail.com>
2017-09-08 00:35:43 +0000

    map練習 減った順に並び替え

--- app.js
+++ app.js
@@ -3,43 +3,44 @@ const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
-
 const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (line) => {
-    const columns = line.split(',');
-    const year = columns[0];
+
+rl.on('line', (lineString) => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
     const prefecture = columns[2];
-    const popu = columns[7];
-    if (year === '2010' || year === '2015') {
+    const popu = parseInt(columns[7]);
+    if (year === 2010 || year === 2015) {
         let value = map.get(prefecture);
         if (!value) {
             value = {
-                popu10: 0,
-                popu15: 0,
+                p10: 0,
+                p15: 0,
                 change: null
             };
         }
-        if (year === '2010') {
-            value.popu10 += parseInt(popu);
+        if (year === 2010) {
+            value.p10 += popu;
         }
-        if (year === '2015') {
-            value.popu15 += parseInt(popu);
+        if (year === 2015) {
+            value.p15 += popu;
         }
         map.set(prefecture, value);
     }
 });
 rl.resume();
+
 rl.on('close', () => {
     for (let pair of map) {
         const value = pair[1];
-        value.change = value.popu15 / value.popu10;
+        value.change = value.p15 / value.p10;
     }
-    // TODO 減った割合のランキングにして順位も一緒に出力するようにしてください
-    const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+    //2010～2015 年にかけて 15〜19 歳の人が減った割合の都道府県ランキング
+    const rankingArray = Array.from(map).sort((p1, p2) => {
+        return p1[1].change - p2[1].change;
     });
-    const rankingStrings = rankingArray.map((pair) => {
-        return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    const rankingStrings = rankingArray.map((p, i) => {
+        return '第' + (i + 1)+ '位 ' + p[0] + ': ' + p[1].p10 + '=>' + p[1].p15 + ' 変化率:' + p[1].change;
     });
     console.log(rankingStrings);
 });
