83b6a2c1c726d657fa30d8ccb2e358bb5f5d3489
meiji20 <meiji2095@yahoo.co.jp>
2017-09-12 15:42:24 +0000

    人が減った割合に修正

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
-
 const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (line) => {
-    const columns = line.split(',');
-    const year = columns[0];
+rl.on('line', (lineString) => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
     const prefecture = columns[2];
-    const popu = columns[7];
-    if (year === '2010' || year === '2015') {
+    const popu = parseInt(columns[7]);
+    if (year === 2010 || year === 2015) {
         let value = map.get(prefecture);
         if (!value) {
             value = {
@@ -19,11 +18,11 @@ rl.on('line', (line) => {
                 change: null
             };
         }
-        if (year === '2010') {
-            value.popu10 += parseInt(popu);
+        if (year === 2010) {
+            value.popu10 += popu;
         }
-        if (year === '2015') {
-            value.popu15 += parseInt(popu);
+        if (year === 2015) {
+            value.popu15 += popu;
         }
         map.set(prefecture, value);
     }
@@ -34,12 +33,12 @@ rl.on('close', () => {
         const value = pair[1];
         value.change = value.popu15 / value.popu10;
     }
-    // TODO 減った割合のランキングにして順位も一緒に出力するようにしてください
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
     const rankingStrings = rankingArray.map((pair) => {
         return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
-    console.log(rankingStrings);
+    const rankingNumber = rankingStrings.map((e, i) => { return (i + 1) + "位 " + e} );
+    console.log(rankingNumber);
 });
