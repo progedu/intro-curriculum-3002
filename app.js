5c4e5cbe83569203ddd7b2d040fe7652d8217b4c
yamamotoshu1127 <yamamotoshu1127@gmail.com>
2017-11-13 11:20:48 +0000

    コメントの追記

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const rl = readline.createInterface({ 'input': rs, 'output': {} });
 const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', (line) => {
     const columns = line.split(',');
+
     const year = columns[0];
     const prefecture = columns[2];
     const popu = columns[7];
+
     if (year === '2010' || year === '2015') {
         let value = map.get(prefecture);
+        // 集計データが存在しない場合は初期化
         if (!value) {
             value = {
                 popu10: 0,
@@ -19,6 +22,7 @@ rl.on('line', (line) => {
                 change: null
             };
         }
+
         if (year === '2010') {
             value.popu10 += parseInt(popu);
         }
@@ -28,16 +32,20 @@ rl.on('line', (line) => {
         map.set(prefecture, value);
     }
 });
+
 rl.resume();
+
 rl.on('close', () => {
+    // 変化率の抽出
     for (let pair of map) {
         const value = pair[1];
         value.change = value.popu15 / value.popu10;
     }
-    // TODO 減った割合のランキングにして順位も一緒に出力するようにしてください
+    // 変化率を減った割合にランク付け
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
         return pair1[1].change - pair2[1].change;
     });
+    // 結果文字列に変換
     const rankingStrings = rankingArray.map((pair, rank) => {
         return (rank + 1) + '位 ' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
