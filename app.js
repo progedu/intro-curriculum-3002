bb00d1c50dbf93b227f60993570b7d041568e92a
Takuma Miura <21156330+tkm-mur@users.noreply.github.com>
2018-01-03 05:31:24 +0900

    2010年から2015年にかけて15〜19歳の人が増えた割合の都道府県ランキング

--- app.js
+++ app.js
@@ -2,16 +2,21 @@
 const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input': rs, 'output': {} });
-
+const rl = readline.createInterface({
+    'input': rs,
+    'output': {}
+});
 const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (line) => {
-    const columns = line.split(',');
-    const year = columns[0];
-    const prefecture = columns[2];
-    const popu = columns[7];
-    if (year === '2010' || year === '2015') {
+
+rl.on('line', (lineString) => {
+    const colums = lineString.split(',');
+    const year = parseInt(colums[0]); // 年
+    const prefecture = colums[2]; // 都道府県
+    const popu = parseInt(colums[7]); // 15〜19歳（人）
+
+    if (year === 2010 || year === 2015) {
         let value = map.get(prefecture);
+
         if (!value) {
             value = {
                 popu10: 0,
@@ -19,12 +24,15 @@ rl.on('line', (line) => {
                 change: null
             };
         }
-        if (year === '2010') {
-            value.popu10 += parseInt(popu);
+
+        if (year === 2010) {
+            value.popu10 += popu;
         }
-        if (year === '2015') {
-            value.popu15 += parseInt(popu);
+
+        if (year === 2015) {
+            value.popu15 += popu;
         }
+
         map.set(prefecture, value);
     }
 });
@@ -34,12 +42,14 @@ rl.on('close', () => {
         const value = pair[1];
         value.change = value.popu15 / value.popu10;
     }
-    // TODO 減った割合のランキングにして順位も一緒に出力するようにしてください
+
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map((pair) => {
-        return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+
+    const rankingStrings = rankingArray.map((pair, rank) => {
+        return (rank + 1) + '位: ' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + '変化率: ' + pair[1].change;
     });
+
     console.log(rankingStrings);
 });
