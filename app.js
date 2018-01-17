d7617eb35579659526fe1efacae5b9abb8d3c93b
7hira <mikoto.hira@gmail.com>
2018-01-17 07:56:30 +0000

    練習問題の実装完了

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
 
-const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+const mapdata = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', (line) => {
     const columns = line.split(',');
     const year = columns[0];
     const prefecture = columns[2];
     const popu = columns[7];
     if (year === '2010' || year === '2015') {
-        let value = map.get(prefecture);
+        let value = mapdata.get(prefecture);
         if (!value) {
             value = {
                 popu10: 0,
@@ -25,21 +25,21 @@ rl.on('line', (line) => {
         if (year === '2015') {
             value.popu15 += parseInt(popu);
         }
-        map.set(prefecture, value);
+        mapdata.set(prefecture, value);
     }
 });
 rl.resume();
 rl.on('close', () => {
-    for (let pair of map) {
+    for (let pair of mapdata) {
         const value = pair[1];
         value.change = value.popu15 / value.popu10;
     }
     // TODO 減った割合のランキングにして順位も一緒に出力するようにしてください
-    const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+    const rankingArray = Array.from(mapdata).sort((pair1, pair2) => {
+        return (pair2[1].change - pair1[1].change) * (-1);
     });
-    const rankingStrings = rankingArray.map((pair) => {
-        return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    const rankingStrings = rankingArray.map((pair,junni) => {
+        return (junni + 1) +'位' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
     console.log(rankingStrings);
 });
