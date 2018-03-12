288434a02ee6d76df23c3a3de485db624fe8ceb3
Taisuke <tie.ver76@gmail.com>
2018-03-12 11:54:29 +0000

    コンフリクトを手動でマージ

--- app.js
+++ app.js
@@ -3,16 +3,6 @@ const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
-<<<<<<< HEAD
-
-const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (line) => {
-    const columns = line.split(',');
-    const year = columns[0];
-    const prefecture = columns[2];
-    const popu = columns[7];
-    if (year === '2010' || year === '2015') {
-=======
 const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', (lineString) => {
     const colums = lineString.split(',');
@@ -20,7 +10,6 @@ rl.on('line', (lineString) => {
     const prefecture = colums[2];
     const popu = parseInt(colums[7]);
     if (year === 2010 || year === 2015) {
->>>>>>> 7b85eabf7250be5d78f4b5fbafd8a6e79c3049a5
         let value = map.get(prefecture);
         if (!value) {
             value = {
@@ -29,19 +18,11 @@ rl.on('line', (lineString) => {
                 change: null
             };
         }
-<<<<<<< HEAD
-        if (year === '2010') {
-            value.popu10 += parseInt(popu);
-        }
-        if (year === '2015') {
-            value.popu15 += parseInt(popu);
-=======
         if (year === 2010) {
             value.popu10 += popu;
         }
         if (year === 2015) {
             value.popu15 += popu;
->>>>>>> 7b85eabf7250be5d78f4b5fbafd8a6e79c3049a5
         }
         map.set(prefecture, value);
     }
@@ -52,17 +33,6 @@ rl.on('close', () => {
         const value = pair[1];
         value.change = value.popu15 / value.popu10;
     }
-<<<<<<< HEAD
-    // TODO 減った割合のランキングにして順位も一緒に出力するようにしてください
-    const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
-    });
-    const rankingStrings = rankingArray.map((pair) => {
-        return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
-    });
-    console.log(rankingStrings);
-});
-=======
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
         return pair1[1].change - pair2[1].change;
     });
@@ -72,4 +42,3 @@ rl.on('close', () => {
     console.log(rankingStrings);
 });
 
->>>>>>> 7b85eabf7250be5d78f4b5fbafd8a6e79c3049a5
