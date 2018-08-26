b30a59549b19318e595e01c92dac804fa3a46c91
Seigoh Sakuma <s.seigoh.0x6198@gmail.com>
2018-08-26 07:55:39 +0000

    練習問題回答

--- app.js
+++ app.js
@@ -11,34 +11,42 @@ rl.on('line', (line) => {
     const prefecture = columns[2];
     const popu = columns[7];
     if (year === '2010' || year === '2015') {
-        let value = map.get(prefecture);
-        if (!value) {
-            value = {
-                popu10: 0,
-                popu15: 0,
-                change: null
-            };
-        }
-        if (year === '2010') {
-            value.popu10 += parseInt(popu);
-        }
-        if (year === '2015') {
-            value.popu15 += parseInt(popu);
-        }
-        map.set(prefecture, value);
+      let value = map.get(prefecture);
+      if (!value) {
+        value = {
+          popu10: 0,
+          popu15: 0,
+          change: null,
+        };
+      }
+
+      if (year === '2010') {
+        value.popu10 += parseInt(popu);
+      }
+
+      if (year === '2015') {
+        value.popu15 += parseInt(popu);
+      }
+
+      map.set(prefecture, value);
     }
 });
+
 rl.resume();
+
 rl.on('close', () => {
     for (let keyAndValue of map) { // keyAndValue の添え字 0 にキー、1 に値が入っている
-        const value = keyAndValue[1];
-        value.change = value.popu15 / value.popu10;
+      const value = keyAndValue[1];
+      value.change = value.popu15 / value.popu10;
     }
+
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+      return pair2[1].change - pair1[1].change;
     });
-    const rankingStrings = rankingArray.map((keyAndValue) => { // keyAndValue の添え字 0 にキー、1 に値が入っている
-        return keyAndValue[0] + ': ' + keyAndValue[1].popu10 + '=>' + keyAndValue[1].popu15 + ' 変化率:' + keyAndValue[1].change;
+
+    const rankingStrings = rankingArray.map((pair, i) => {
+      return (i + 1) + '位 ' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
+
     console.log(rankingStrings);
 });
