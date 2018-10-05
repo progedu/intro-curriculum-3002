45690a9a83f832b103b09a468cc056869d80460c
Hi928 <hiroki.i928@gmail.com>
2018-10-05 13:58:23 +0900

    人が減った割合のランキングに修正

--- app.js
+++ app.js
@@ -36,8 +36,9 @@ rl.on('close', () => {
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
         return pair2[1].change - pair1[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankigArray.map((pair, i) => {
+        return (i + 1) + '位 ' + pair[0] + ': ' + pair[1].popu10
+    + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
     console.log(rankingStrings);
 });
