46ada3e0a3492ce7649ac491f59c9602cfe3faa8
d5500 <40521621+d5500@users.noreply.github.com>
2018-10-07 20:59:39 +0900

    人が減った割合のランキング

--- app.js
+++ app.js
@@ -30,14 +30,15 @@ rl.on('line', (line) => {
 });
 rl.resume();
 rl.on('close', () => {
-    for (let [key, value] of prefectureDataMap) {
+    for (let pair of map) {
+        const value = pair[1];
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map((pair, i) => {
+        return (i + 1) + '位 ' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
     console.log(rankingStrings);
 });
