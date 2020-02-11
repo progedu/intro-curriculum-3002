69048faadd78122e69e8af295bde6733f3a329a6
yuka nakamura <catcheridrop@gmail.com>
2020-02-11 16:23:05 +0900

    インデントの修正

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ rl.on('close', () => {
   for (let [key, value] of prefectureDataMap) { 
     value.change = value.popu15 / value.popu10;
   }
-	const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-			return pair1[1].change - pair2[1].change;
-	});
-	const rankingStrings = rankingArray.map(([key, value], num) => {
-			return (num + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
-	});
-	console.log(rankingStrings);
+  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+      return pair1[1].change - pair2[1].change;
+  });
+  const rankingStrings = rankingArray.map(([key, value], num) => {
+      return (num + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+  });
+  console.log(rankingStrings);
 });
