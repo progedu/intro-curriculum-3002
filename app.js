c126ce7c6f52a5592dcd1232087e8cfd7028449e
sasami-pasta <tmnr0715@docomo.ne.jp>
2020-03-13 15:31:59 +0900

    昇順順位付きランキング

--- app.js
+++ app.js
@@ -32,10 +32,11 @@ rl.on('close', () => {
     value.change = value.popu15 / value.popu10;
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    return -(pair2[1].change - pair1[1].change);
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+  //map関数に渡す引数を二つにすると、第二引数はインデックス番号になる
+  const rankingStrings = rankingArray.map(([key, value], i) => {
+    return (i + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
   });
   console.log(rankingStrings);
 });
