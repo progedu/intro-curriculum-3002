c710d0dc503de198abc32f94cdcb2e1cff9bf80d
o8ep3 <o8ep3@yahoo.co.jp>
2020-04-16 22:51:30 +0900

    逆順ソートの実装

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ rl.on('line', (lineString) => {
     }
     prefectureDataMap.set(prefecture, value);
   }
-});
+
 rl.on('close', () => {
   for (let [key, value] of prefectureDataMap) { 
     value.change = value.popu15 / value.popu10;
@@ -34,8 +34,8 @@ rl.on('close', () => {
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
     return pair2[1].change - pair1[1].change;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+  const rankingStrings = rankingArray.map(([key, value], i) => {
+    return (i + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
   });
   console.log(rankingStrings);
 });
