1cdddf0422d9f5dfed36199a4ce0499d4d8ca8c4
hashi-oki <sushikuite77@gmail.com>
2020-05-17 15:39:47 +0900

    ランキングを低い順に変更

--- app.js
+++ app.js
@@ -5,37 +5,55 @@ const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
 const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', (lineString) => {
-  const columns = lineString.split(',');
-  const year = parseInt(columns[0]);
-  const prefecture = columns[1];
-  const popu = parseInt(columns[3]);
-  if (year === 2010 || year === 2015) {
-    let value = prefectureDataMap.get(prefecture);
-    if (!value) {
-      value = {
-        popu10: 0,
-        popu15: 0,
-        change: null
-      };
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[1];
+    const popu = parseInt(columns[3]);
+    if (year === 2010 || year === 2015) {
+        let value = prefectureDataMap.get(prefecture);
+        if (!value) {
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        prefectureDataMap.set(prefecture, value);
     }
-    if (year === 2010) {
-      value.popu10 = popu;
-    }
-    if (year === 2015) {
-      value.popu15 = popu;
-    }
-    prefectureDataMap.set(prefecture, value);
-  }
 });
 rl.on('close', () => {
-  for (let [key, value] of prefectureDataMap) { 
-    value.change = value.popu15 / value.popu10;
-  }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
-  });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
-  });
-  console.log(rankingStrings);
+    for (let [key, value] of prefectureDataMap) {
+        value.change = value.popu15 / value.popu10;
+    }
+
+    /*
+  比較関数は 2 つの引数を受けとって、
+  前者の引数 pair1 を 後者の引数 pair2 より前にしたいときは、負の整数、
+  pair2 を pair1 より前にしたいときは、正の整数、
+  pair1 と pair2 の並びをそのままにしたいときは 0 を返す必要があります。
+  */
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+        return pair1[1].change - pair2[1].change;
+    });
+
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return (i + 1) +
+            '位' +
+            key +
+            ': ' +
+            value.popu10 +
+            '=>' +
+            value.popu15 +
+            ' 変化率:' +
+            value.change
+
+    });
+
+    console.log(rankingStrings);
 });
