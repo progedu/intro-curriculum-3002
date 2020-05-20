e9cee4e16c03605747356a0d1ec58641276629bb
Trskey <trskey@gmail.com>
2020-05-20 12:21:33 +0900

    減った割合ランキングの実装完了

--- app.js
+++ app.js
@@ -32,10 +32,11 @@ rl.on('close', () => {
     value.change = value.popu15 / value.popu10;
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    return pair1[1].change - pair2[1].change;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+  const rankingStrings = rankingArray.map(([key, value], i) => {
+    return i + 1 + '位: ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
   });
+  console.log("2010年から2015年にかけて、15〜19歳の人が減った割合の都道府県ランキング");
   console.log(rankingStrings);
 });
