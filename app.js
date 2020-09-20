471450983db8ae11feb87e51fba4e1d04cb359a6
Masayu1224 <masayu.hiroayu@gmail.com>
2020-09-20 06:22:54 +0000

    ランキング逆順

--- app.js
+++ app.js
@@ -29,14 +29,15 @@ rl.on('line', (line) => {
     }
 });
 rl.on('close', () => {
-    for (let [key, value] of prefectureDataMap) {
+    for (let pair of map) {
+        const value = pair[1];
         value.change = value.popu15 / value.popu10;
     }
-    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+    const rankingArray = Array.from(map).sort((pair1, pair2) => {
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map((pair, i) => {
+        return (i + 1) + '位 ' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
     console.log(rankingStrings);
 });
