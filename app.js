35f7ffded6d4938d44a91fcc35f1718378d49f3a
Takeo Haseoka <thaseoka@gmail.com>
2021-01-03 06:09:41 +0000

    人が減った割合

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
 
-const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+//const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+
 rl.on('line', (line) => {
     const columns = line.split(',');
     const year = columns[0];
     const prefecture = columns[2];
     const popu = columns[7];
     if (year === '2010' || year === '2015') {
-        let value = prefectureDataMap.get(prefecture);
-        if (!value) {
+//        let value = prefectureDataMap.get(prefecture);
+        let value = map.get(prefecture);
+    if (!value) {
             value = {
                 popu10: 0,
                 popu15: 0,
@@ -25,18 +28,20 @@ rl.on('line', (line) => {
         if (year === '2015') {
             value.popu15 += parseInt(popu);
         }
-        prefectureDataMap.set(prefecture, value);
+//        prefectureDataMap.set(prefecture, value);
+        map.set(prefecture, value);
     }
 });
 rl.on('close', () => {
-    for (let [key, value] of prefectureDataMap) {
+    for (let pair of map) {
+        const value = pair[1];
         value.change = value.popu15 / value.popu10;
     }
-    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+    const rankingArray = Array.from(map).sort((pair1, pair2) => {
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map((pair, i) => {
+        return (i + 1)+ '位 ' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
     console.log(rankingStrings);
 });
