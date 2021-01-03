f660414eef93adf4926a47f56ad602036e702c22
Yusuke Tokura <t-on-yusu@orchid.plala.or.jp>
2021-01-03 09:04:57 +0900

    逆順

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
 'use strict';
-const fs = require('fs');
-const readline = require('readline');
-const rs = fs.ReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input': rs, 'output': {} });
+// require は、Node.js のモジュールを呼び出し
+const fs = require('fs'); // fs はファイルシステムを扱うモジュール
+const readline = require('readline'); // readline はファイルを一行ずつ読み込むモジュール
+const rs = fs.createReadStream('./popu-pref.csv');
+// createReadStream でファイル読み込みを行うStream生成
 
-const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (line) => {
-    const columns = line.split(',');
-    const year = columns[0];
-    const prefecture = columns[2];
-    const popu = columns[7];
-    if (year === '2010' || year === '2015') {
-        let value = prefectureDataMap.get(prefecture);
-        if (!value) {
-            value = {
-                popu10: 0,
-                popu15: 0,
-                change: null
-            };
-        }
-        if (year === '2010') {
-            value.popu10 += parseInt(popu);
-        }
-        if (year === '2015') {
-            value.popu15 += parseInt(popu);
-        }
-        prefectureDataMap.set(prefecture, value);
+const rl = readline.createInterface({ input: rs, output: {} });
+// 上のStream を、readline オブジェクトの入力として、rl オブジェクトを作成？
+
+const prefdata_map = new Map();
+
+rl.on('line', lineString => {   //line イベント発生時に以下を発動
+  const pop_array = lineString.split(',');
+  const year = parseInt(pop_array[0]);
+  const pref = pop_array[1]
+  const pop_num = parseInt(pop_array[3]);
+
+  if (year === 2010 || year === 2015) {
+    let value = prefdata_map.get(pref);
+    if (!value) {
+      value = {
+        pop10: 0,
+        pop15: 0,
+        change: null
+      };
     }
+    if (year === 2010) {
+      value.pop10 = pop_num;
+    }
+    if (year === 2015) {
+      value.pop15 = pop_num;
+    }
+    prefdata_map.set(pref, value);
+  }
 });
+
+
+
 rl.on('close', () => {
-    for (let [key, value] of prefectureDataMap) {
-        value.change = value.popu15 / value.popu10;
-    }
-    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
-    });
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
-    });
-    console.log(rankingStrings);
+  for (let [key, value] of prefdata_map) {
+    value.change = value.pop15 / value.pop10;
+  }
+  const ranking_array = Array.from(prefdata_map).sort((pair1, pair2) => { // 2つの引数の大小でsort
+    return -pair2[1].change + pair1[1].change;
+  })
+  // 以下は単なる配列の整形だが、ここでmap関数（Mapの各要素に作用）を利用
+  const ranking_strings = ranking_array.map(([key, value],i) => {
+    return (' 第'+(i+1)+'位' + key + ':' + value.pop10 + '=>' + value.pop15 + '変化:' + value.change);
+  });
+  console.log(ranking_strings);
 });
