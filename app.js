764e4c341253a137cdabf30527c83b8e373d9a80
Kengo Yagishita <yagishitakengo1002@gmail.com>
2021-05-03 23:30:58 +0900

    ランキングを昇順に変更

--- app.js
+++ app.js
@@ -31,11 +31,12 @@ rl.on('close', () => {
   for (let [key, value] of prefectureDataMap) { 
     value.change = value.popu15 / value.popu10;
   }
+  // 連想配列を普通の配列に変換し、sort関数でchangeの昇順に並び変える
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    return pair1[1].change - pair2[1].change;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+  const rankingStrings = rankingArray.map(([key, value], i) => {
+    return '第' + (i+1) + '位: ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
   });
   console.log(rankingStrings);
 });
